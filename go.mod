fbbde65d8aaa7d5277dde3a08f517c3f46ad0e10
afushimi-source <67850944+afushimi-source@users.noreply.github.com>
2022-09-24 18:37:21 +0900

    機能をテストしやすいように分割

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/afushimi-source/go_todo_app
+
+go 1.19
+
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	golang.org/x/sync v0.0.0-20220907140024-f12130a52804
+)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/afushimi-source/go_todo_app/config"
+	"golang.org/x/sync/errgroup"
+)
+
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to teminate server: %v", err)
+		os.Exit(1)
+	}
+}
